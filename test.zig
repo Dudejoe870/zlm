27bd6a5f416b2582ed20bebc7f3a03da7b39100b
Dudejoe870 <dudejoe870@gmail.com>
2022-11-19 14:44:53 -0600

    Change testing to use expect instead of assert

--- test.zig
+++ test.zig
@@ -1,6 +1,7 @@
 const std = @import("std");
 const math = @import("zlm.zig");
-const assert = std.debug.assert;
+const expect = std.testing.expect;
+const expectEqual = std.testing.expectEqual;
 
 const vec2 = math.vec2;
 const vec3 = math.vec3;
@@ -15,86 +16,86 @@ const Mat4 = math.Mat4;
 
 test "default generic is f32" {
     const T = @TypeOf(vec2(1, 2).x);
-    try std.testing.expectEqual(T, f32);
+    try expectEqual(T, f32);
 }
 
 test "SpecializeOn()" {
     const math_u64 = math.SpecializeOn(u64);
     const T = @TypeOf(math_u64.vec2(3, 4).x);
-    try std.testing.expectEqual(T, u64);
+    try expectEqual(T, u64);
 }
 
 test "constructors" {
     const v2 = vec2(1, 2);
-    assert(v2.x == 1);
-    assert(v2.y == 2);
+    try expect(v2.x == 1);
+    try expect(v2.y == 2);
 
     const v3 = vec3(1, 2, 3);
-    assert(v3.x == 1);
-    assert(v3.y == 2);
-    assert(v3.z == 3);
+    try expect(v3.x == 1);
+    try expect(v3.y == 2);
+    try expect(v3.z == 3);
 
     const v4 = vec4(1, 2, 3, 4);
-    assert(v4.x == 1);
-    assert(v4.y == 2);
-    assert(v4.z == 3);
-    assert(v4.w == 4);
+    try expect(v4.x == 1);
+    try expect(v4.y == 2);
+    try expect(v4.z == 3);
+    try expect(v4.w == 4);
 }
 
 test "vec2 arithmetics" {
     const a = vec2(2, 1);
     const b = vec2(1, 2);
 
-    assert(std.meta.eql(Vec2.add(a, b), vec2(3, 3)));
-    assert(std.meta.eql(Vec2.sub(a, b), vec2(1, -1)));
-    assert(std.meta.eql(Vec2.mul(a, b), vec2(2, 2)));
-    assert(std.meta.eql(Vec2.div(a, b), vec2(2, 0.5)));
-    assert(std.meta.eql(Vec2.div_scalar(a, 2.0), vec2(1, 0.5)));
-    assert(std.meta.eql(Vec2.scale(a, 2.0), vec2(4, 2)));
+    try expect(std.meta.eql(Vec2.add(a, b), vec2(3, 3)));
+    try expect(std.meta.eql(Vec2.sub(a, b), vec2(1, -1)));
+    try expect(std.meta.eql(Vec2.mul(a, b), vec2(2, 2)));
+    try expect(std.meta.eql(Vec2.div(a, b), vec2(2, 0.5)));
+    try expect(std.meta.eql(Vec2.div_scalar(a, 2.0), vec2(1, 0.5)));
+    try expect(std.meta.eql(Vec2.scale(a, 2.0), vec2(4, 2)));
 
-    assert(Vec2.dot(a, b) == 4.0);
+    try expect(Vec2.dot(a, b) == 4.0);
 
-    assert(Vec2.length2(a) == 5.0);
-    assert(Vec2.length(a) == std.math.sqrt(5.0));
-    assert(Vec2.length(b) == std.math.sqrt(5.0));
+    try expect(Vec2.length2(a) == 5.0);
+    try expect(Vec2.length(a) == std.math.sqrt(5.0));
+    try expect(Vec2.length(b) == std.math.sqrt(5.0));
 }
 
 test "vec3 arithmetics" {
     const a = vec3(2, 1, 3);
     const b = vec3(1, 2, 3);
 
-    assert(std.meta.eql(Vec3.add(a, b), vec3(3, 3, 6)));
-    assert(std.meta.eql(Vec3.sub(a, b), vec3(1, -1, 0)));
-    assert(std.meta.eql(Vec3.mul(a, b), vec3(2, 2, 9)));
-    assert(std.meta.eql(Vec3.div(a, b), vec3(2, 0.5, 1)));
-    assert(std.meta.eql(Vec3.div_scalar(a, 2.0), vec3(1, 0.5, 1.5)));
-    assert(std.meta.eql(Vec3.scale(a, 2.0), vec3(4, 2, 6)));
+    try expect(std.meta.eql(Vec3.add(a, b), vec3(3, 3, 6)));
+    try expect(std.meta.eql(Vec3.sub(a, b), vec3(1, -1, 0)));
+    try expect(std.meta.eql(Vec3.mul(a, b), vec3(2, 2, 9)));
+    try expect(std.meta.eql(Vec3.div(a, b), vec3(2, 0.5, 1)));
+    try expect(std.meta.eql(Vec3.div_scalar(a, 2.0), vec3(1, 0.5, 1.5)));
+    try expect(std.meta.eql(Vec3.scale(a, 2.0), vec3(4, 2, 6)));
 
-    assert(Vec3.dot(a, b) == 13.0);
+    try expect(Vec3.dot(a, b) == 13.0);
 
-    assert(Vec3.length2(a) == 14.0);
-    assert(Vec3.length(a) == std.math.sqrt(14.0));
-    assert(Vec3.length(b) == std.math.sqrt(14.0));
+    try expect(Vec3.length2(a) == 14.0);
+    try expect(Vec3.length(a) == std.math.sqrt(14.0));
+    try expect(Vec3.length(b) == std.math.sqrt(14.0));
 
-    assert(std.meta.eql(Vec3.cross(vec3(1, 2, 3), vec3(-7, 8, 9)), vec3(-6, -30, 22)));
+    try expect(std.meta.eql(Vec3.cross(vec3(1, 2, 3), vec3(-7, 8, 9)), vec3(-6, -30, 22)));
 }
 
 test "vec4 arithmetics" {
     const a = vec4(2, 1, 4, 3);
     const b = vec4(1, 2, 3, 4);
 
-    assert(std.meta.eql(Vec4.add(a, b), vec4(3, 3, 7, 7)));
-    assert(std.meta.eql(Vec4.sub(a, b), vec4(1, -1, 1, -1)));
-    assert(std.meta.eql(Vec4.mul(a, b), vec4(2, 2, 12, 12)));
-    assert(std.meta.eql(Vec4.div(a, b), vec4(2, 0.5, 4.0 / 3.0, 0.75)));
-    assert(std.meta.eql(Vec4.div_scalar(a, 2.0), vec4(1, 0.5, 2.0, 1.5)));
-    assert(std.meta.eql(Vec4.scale(a, 2.0), vec4(4, 2, 8, 6)));
+    try expect(std.meta.eql(Vec4.add(a, b), vec4(3, 3, 7, 7)));
+    try expect(std.meta.eql(Vec4.sub(a, b), vec4(1, -1, 1, -1)));
+    try expect(std.meta.eql(Vec4.mul(a, b), vec4(2, 2, 12, 12)));
+    try expect(std.meta.eql(Vec4.div(a, b), vec4(2, 0.5, 4.0 / 3.0, 0.75)));
+    try expect(std.meta.eql(Vec4.div_scalar(a, 2.0), vec4(1, 0.5, 2.0, 1.5)));
+    try expect(std.meta.eql(Vec4.scale(a, 2.0), vec4(4, 2, 8, 6)));
 
-    assert(Vec4.dot(a, b) == 28.0);
+    try expect(Vec4.dot(a, b) == 28.0);
 
-    assert(Vec4.length2(a) == 30.0);
-    assert(Vec4.length(a) == std.math.sqrt(30.0));
-    assert(Vec4.length(b) == std.math.sqrt(30.0));
+    try expect(Vec4.length2(a) == 30.0);
+    try expect(Vec4.length(a) == std.math.sqrt(30.0));
+    try expect(Vec4.length(b) == std.math.sqrt(30.0));
 }
 
 test "vec3 <-> vec4 interop" {
@@ -102,11 +103,11 @@ test "vec3 <-> vec4 interop" {
     const pos = vec4(1, 2, 3, 1);
     const dir = vec4(1, 2, 3, 0);
 
-    assert(std.meta.eql(Vec3.toAffinePosition(v), pos));
-    assert(std.meta.eql(Vec3.toAffineDirection(v), dir));
+    try expect(std.meta.eql(Vec3.toAffinePosition(v), pos));
+    try expect(std.meta.eql(Vec3.toAffineDirection(v), dir));
 
-    assert(std.meta.eql(Vec3.fromAffinePosition(pos), v));
-    assert(std.meta.eql(Vec3.fromAffineDirection(dir), v));
+    try expect(std.meta.eql(Vec3.fromAffinePosition(pos), v));
+    try expect(std.meta.eql(Vec3.fromAffineDirection(dir), v));
 }
 
 // TODO: write tests for mat2, mat3
@@ -183,15 +184,15 @@ test "mat4 arithmetics" {
     };
 
     // make sure basic properties are not messed up
-    assert(std.meta.eql(Mat4.mul(id, id), id));
-    assert(std.meta.eql(Mat4.mul(mat, id), mat));
-    assert(std.meta.eql(Mat4.mul(id, mat), mat));
+    try expect(std.meta.eql(Mat4.mul(id, id), id));
+    try expect(std.meta.eql(Mat4.mul(mat, id), mat));
+    try expect(std.meta.eql(Mat4.mul(id, mat), mat));
 
-    assert(std.meta.eql(Mat4.mul(mat, mat), mat_mult_by_mat_by_hand));
-    assert(std.meta.eql(Mat4.mul(mat_a, mat_b), mat_a_times_b));
-    assert(std.meta.eql(Mat4.mul(mat_b, mat_a), mat_b_times_a));
+    try expect(std.meta.eql(Mat4.mul(mat, mat), mat_mult_by_mat_by_hand));
+    try expect(std.meta.eql(Mat4.mul(mat_a, mat_b), mat_a_times_b));
+    try expect(std.meta.eql(Mat4.mul(mat_b, mat_a), mat_b_times_a));
 
-    assert(std.meta.eql(Mat4.transpose(mat), mat_transposed));
+    try expect(std.meta.eql(Mat4.transpose(mat), mat_transposed));
 }
 // zig fmt: on
 
@@ -220,49 +221,49 @@ test "vec4 transform" {
 
     const vec = vec4(1, 2, 3, 4);
 
-    assert(std.meta.eql(Vec4.transform(vec, mat), vec4(90, 100, 110, 120)));
-    assert(std.meta.eql(Vec4.transform(vec4(1, 2, 3, 1), transform), vec4(12, 24, 36, 1)));
-    assert(std.meta.eql(Vec4.transform(vec4(1, 2, 3, 0), transform), vec4(2, 4, 6, 0)));
+    try expect(std.meta.eql(Vec4.transform(vec, mat), vec4(90, 100, 110, 120)));
+    try expect(std.meta.eql(Vec4.transform(vec4(1, 2, 3, 1), transform), vec4(12, 24, 36, 1)));
+    try expect(std.meta.eql(Vec4.transform(vec4(1, 2, 3, 0), transform), vec4(2, 4, 6, 0)));
 }
 
 test "vec2 swizzle" {
-    assert(std.meta.eql(vec4(0, 1, 1, 2), vec2(1, 2).swizzle("0x1y")));
-    assert(std.meta.eql(vec2(2, 1), vec2(1, 2).swizzle("yx")));
+    try expect(std.meta.eql(vec4(0, 1, 1, 2), vec2(1, 2).swizzle("0x1y")));
+    try expect(std.meta.eql(vec2(2, 1), vec2(1, 2).swizzle("yx")));
 }
 
 test "vec3 swizzle" {
-    assert(std.meta.eql(vec4(1, 1, 2, 3), vec3(1, 2, 3).swizzle("xxyz")));
-    assert(std.meta.eql(vec2(3, 3), vec3(1, 2, 3).swizzle("zz")));
+    try expect(std.meta.eql(vec4(1, 1, 2, 3), vec3(1, 2, 3).swizzle("xxyz")));
+    try expect(std.meta.eql(vec2(3, 3), vec3(1, 2, 3).swizzle("zz")));
 }
 
 test "vec4 swizzle" {
-    assert(std.meta.eql(vec4(3, 4, 2, 1), vec4(1, 2, 3, 4).swizzle("zwyx")));
-    assert(std.meta.eql(@as(f32, 3), vec4(1, 2, 3, 4).swizzle("z")));
+    try expect(std.meta.eql(vec4(3, 4, 2, 1), vec4(1, 2, 3, 4).swizzle("zwyx")));
+    try expect(std.meta.eql(@as(f32, 3), vec4(1, 2, 3, 4).swizzle("z")));
 }
 
 test "vec2 get" {
-    assert(vec2(3, 6).get(0) == 3);
-    assert(vec2(3, 6).get(1) == 6);
+    try expect(vec2(3, 6).get(0) == 3);
+    try expect(vec2(3, 6).get(1) == 6);
 }
 
 test "vec3 get" {
-    assert(vec3(3, 6, 5).get(0) == 3);
-    assert(vec3(3, 6, 5).get(1) == 6);
-    assert(vec3(3, 6, 5).get(2) == 5);
+    try expect(vec3(3, 6, 5).get(0) == 3);
+    try expect(vec3(3, 6, 5).get(1) == 6);
+    try expect(vec3(3, 6, 5).get(2) == 5);
 }
 
 test "vec4 get" {
-    assert(vec4(3, 6, 5, 4).get(0) == 3);
-    assert(vec4(3, 6, 5, 4).get(1) == 6);
-    assert(vec4(3, 6, 5, 4).get(2) == 5);
-    assert(vec4(3, 6, 5, 4).get(3) == 4);
+    try expect(vec4(3, 6, 5, 4).get(0) == 3);
+    try expect(vec4(3, 6, 5, 4).get(1) == 6);
+    try expect(vec4(3, 6, 5, 4).get(2) == 5);
+    try expect(vec4(3, 6, 5, 4).get(3) == 4);
 }
 
 test "vec2 set" {
     var vec = Vec2.zero;
     vec.set(0, 3);
     vec.set(1, 6);
-    assert(std.meta.eql(vec2(3, 6), vec));
+    try expect(std.meta.eql(vec2(3, 6), vec));
 }
 
 test "vec3 set" {
@@ -270,7 +271,7 @@ test "vec3 set" {
     vec.set(0, 3);
     vec.set(1, 6);
     vec.set(2, 7);
-    assert(std.meta.eql(vec3(3, 6, 7), vec));
+    try expect(std.meta.eql(vec3(3, 6, 7), vec));
 }
 
 test "vec4 set" {
@@ -279,35 +280,35 @@ test "vec4 set" {
     vec.set(1, 6);
     vec.set(2, 7);
     vec.set(3, 5);
-    assert(std.meta.eql(vec4(3, 6, 7, 5), vec));
+    try expect(std.meta.eql(vec4(3, 6, 7, 5), vec));
 }
 
 test "vec2 cast" {
     const vec = vec2(0.5, 8.0);
-    assert(std.meta.eql(vec.lossyCast(i32), math.SpecializeOn(i32).vec2(0, 8)));
-    assert(std.meta.eql(vec.bitCast(u32), math.SpecializeOn(u32).vec2(
+    try expect(std.meta.eql(vec.lossyCast(i32), math.SpecializeOn(i32).vec2(0, 8)));
+    try expect(std.meta.eql(vec.bitCast(u32), math.SpecializeOn(u32).vec2(
         @bitCast(u32, @as(f32, 0.5)), 
         @bitCast(u32, @as(f32, 8.0)))));
-    assert(std.meta.eql(vec.lossyCast(f16), math.SpecializeOn(f16).vec2(0.5, 8.0)));
+    try expect(std.meta.eql(vec.lossyCast(f16), math.SpecializeOn(f16).vec2(0.5, 8.0)));
 }
 
 test "vec3 cast" {
     const vec = vec3(0.5, 8.0, -8.0);
-    assert(std.meta.eql(vec.lossyCast(i32), math.SpecializeOn(i32).vec3(0, 8, -8)));
-    assert(std.meta.eql(vec.bitCast(u32), math.SpecializeOn(u32).vec3(
+    try expect(std.meta.eql(vec.lossyCast(i32), math.SpecializeOn(i32).vec3(0, 8, -8)));
+    try expect(std.meta.eql(vec.bitCast(u32), math.SpecializeOn(u32).vec3(
         @bitCast(u32, @as(f32, 0.5)), 
         @bitCast(u32, @as(f32, 8.0)),
         @bitCast(u32, @as(f32, -8.0)))));
-    assert(std.meta.eql(vec.lossyCast(f16), math.SpecializeOn(f16).vec3(0.5, 8.0, -8.0)));
+    try expect(std.meta.eql(vec.lossyCast(f16), math.SpecializeOn(f16).vec3(0.5, 8.0, -8.0)));
 }
 
 test "vec4 cast" {
     const vec = vec4(0.5, -256.0, -8.0, -0.5);
-    assert(std.meta.eql(vec.lossyCast(i8), math.SpecializeOn(i8).vec4(0, std.math.minInt(i8), -8, 0)));
-    assert(std.meta.eql(vec.bitCast(u32), math.SpecializeOn(u32).vec4(
+    try expect(std.meta.eql(vec.lossyCast(i8), math.SpecializeOn(i8).vec4(0, std.math.minInt(i8), -8, 0)));
+    try expect(std.meta.eql(vec.bitCast(u32), math.SpecializeOn(u32).vec4(
         @bitCast(u32, @as(f32, 0.5)), 
         @bitCast(u32, @as(f32, -256.0)),
         @bitCast(u32, @as(f32, -8.0)),
         @bitCast(u32, @as(f32, -0.5)))));
-    assert(std.meta.eql(vec.lossyCast(f16), math.SpecializeOn(f16).vec4(0.5, -256.0, -8.0, -0.5)));
+    try expect(std.meta.eql(vec.lossyCast(f16), math.SpecializeOn(f16).vec4(0.5, -256.0, -8.0, -0.5)));
 }
